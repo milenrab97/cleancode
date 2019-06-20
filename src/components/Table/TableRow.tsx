ab6c4572e43bbba6ae0e9a0aef05513f72d9ee8f
Milen Rabadzhiev <milenrab97@gmail.com>
2019-06-21 02:47:30 +0300

    chore: remove empty line

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { columnDef } from "../../contracts/global";
 
-
 export interface TableRowProps {
   currentRow: any;
   columnDefs: columnDef[];
